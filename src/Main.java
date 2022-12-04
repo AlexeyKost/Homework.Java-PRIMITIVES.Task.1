f8699a47c7b485d31a57004f32717d291ec9b5bb
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2022-12-04 21:29:33 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amout = 1050; //стоимость покупки
+
+        int bonus = amout / 20; //мили
+
+        System.out.println(bonus + " мили ");
+    }
+}
